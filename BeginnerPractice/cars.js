95b150386524e3c8a00c76b0ceaebf5e0a27a195
ItsFerny <ThatFuryFellow@gmail.com>
2017-06-19 09:40:42 -0400

    Cars simulation

---
+++ BeginnerPractice/cars.js
@@ -0,0 +1,68 @@
+class Car {
+  constructor(name, efficiency) {
+  this.name = name;
+  this.efficiency = efficiency;
+  this.fuel = 100;
+  this.speed = 0;
+  this.position = 0;
+
+  }
+accelerate() {
+  this.fuel -= this.efficiency;
+  this.speed += 1;
+  }
+
+move() {
+  this.position += this.speed;
+              }
+
+stop() {
+  this.speed = 0;
+ }
+}
+
+class RaceSim {
+  constructor(cars) {
+    this.cars = cars;
+  }
+
+  simulateRound() {
+    for(var i = 0; i < this.cars.length; i++) {
+      var c = this.cars[i];
+      if(c.fuel > 0) {
+        c.accelerate();
+        c.move();
+      } else {
+      c.stop();
+      }
+    }
+  }
+
+  checkAllOut() { //checks if anyone has fuel
+    var allOut = true;
+    for (var i = 0; i < this.cars.length; i++) {
+      var c = this.cars[i];
+      if(c.fuel > 0) {
+        allOut = false;
+      }
+    }
+    return allOut;
+  }
+
+  simulate() {
+while(!this.checkAllOut()) {
+  this.simulateRound();
+}
+for(var i = 0; i < this.cars.length; i++) {
+  console.log(this.cars[i]);
+}
+}
+}
+
+var car1 = new Car("Fast", 3);
+var car2 = new Car("SlowNSteady", 1);
+
+var carArray = [car1, car2];
+var race = new RaceSim(carArray);
+
+race.simulate();
--- BeginnerPractice/index.js
+++ BeginnerPractice/index.js
@@ -2,14 +2,38 @@ var a = 50;
 var b = 100;
 
 var add = function(a, b) {
-  return a + b;
+  // return a + b;
+  var result = a;
+  for (var i = 0; i < b; i+=1) {
+  result +=1;
+}
+return result;
 }
 
 var subtract = function(a, b){
   return a - b;
 }
 
-console.log(add(a, b));
-console.log(add(b, b));
-console.log(subtract(a, b));
-console.log(subtract(a, a));
+var multiply = function(a, b) {
+  return a * b; // * = shift + 8
+}
+
+var divide = function(a, b) {
+  return a / b;
+}
+
+
+class Calculator {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+      }
+
+
+add(x) {
+  return this.a + this.b + x;
+}
+}
+
+var c = new Calculator(50, 100);
+console.log(c.add(50));
